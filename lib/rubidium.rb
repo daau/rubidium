abf60acc6984c732b756eaf83d8b176765b813be
Daniel Au <daniel.au@scouttalent.ca>
2018-10-16 21:44:40 -0700

    Exception handling

--- lib/rubidium.rb
+++ lib/rubidium.rb
@@ -1,13 +1,18 @@
 require "rubidium/version"
 require "rubidium/routing"
 require "rubidium/array"
+require "pry-byebug"
 
 module Rubidium
   class Application
     def call(env)
-      klass, action = get_controller_and_action(env)
-      controller = klass.new(env)
-      response = controller.send(action)
+      begin
+        klass, action = get_controller_and_action(env)
+        controller = klass.new(env)
+        response = controller.send(action)
+      rescue
+        return [404, {"Content-Type" => "text/html"}, ["Bark bark! Not Found"]]
+      end
       [200, {"Content-Type" => "text/html"}, [response]]
     end
   end
--- rubidium.gemspec
+++ rubidium.gemspec
@@ -36,6 +36,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
-  spec.add_development_dependency "pry-byebug"
+  spec.add_runtime_dependency "pry-byebug"
   spec.add_runtime_dependency "rack"
 end
